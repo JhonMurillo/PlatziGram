f34401435605f5bd2002610af4d33eb84e88d812
Jhon Mario Murillo Cordoba <jhonmurillo2014@gmail.com>
2020-02-05 23:06:10 -0500

    Finish the class-based views Class

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,31 @@
 from django.urls import path
 from users import views 
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('login', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
-    path('signup', views.signup, name='signup'),
-    path('me', views.update_profile, name='update_profile'),
+    path(
+        route= 'login', 
+        view= views.LoginView.as_view(),
+        name='login'
+    ),
+    path(
+        route='logout', 
+        view= views.LogoutView.as_view(),
+        name='logout'
+    ),
+    path(
+        route='signup',
+        view = views.SignupView.as_view(),
+        name='signup')
+    ,
+    path(
+        route='me',
+        view= views.UpdateProfileView.as_view(), 
+        name='update_profile'
+    ),
 
     path(
         route='<str:username>/',
-        view = TemplateView.as_view(template_name='users/detail.html'),
+        view = views.UserDetailView.as_view(),
         name='detail'
     )
 ]
